15db92dc3d0b44427a41e12bdfc828c5089bdd1c
erikorojo <14225024+erikorojo@users.noreply.github.com>
2018-12-09 11:46:56 -0600

    Ch 2: Basics in React, Sec: Interactions with Forms and Events

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item =>
+      item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -30,6 +33,7 @@ class App extends Component {
       // I.e. "list" instead of "list: list"
       //
       list: list,
+      searchTerm: '',
     };
 
     // this.onDismiss = this.onDismiss.bind(this);
@@ -47,33 +51,43 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
-                             <div key={item.objectID}>
-                               <span>
-                                 <a href={item.url}>{item.title}</a>
-                               </span>
-                               <span>{item.author}</span>
-                               <span>{item.num_comments}</span>
-                               <span>{item.points}</span>
-                               <span>
-                                 <button
-                             // It's important we bind a function,
-                             // i.e. defined below via ES6 arrow syntax,
-                             // to the onClick event instead of just the code
-                             // expression. Otherwise, the code gets evaluated
-                             // upon page load but not when clicking the
-                             // button.
-                                   onClick={() => this.onDismiss(item.objectID)}
-                                   type="button"
-                                 >
-                                   Dismiss
-                                 </button>
-                               </span>
-                             </div>
-                            )}
+        <form>
+          <input type="text"
+                 onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(
+          item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+          // It's important we bind a function,
+          // i.e. defined below via ES6 arrow syntax,
+          // to the onClick event instead of just the code
+          // expression. Otherwise, the code gets evaluated
+          // upon page load but not when clicking the
+          // button.
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
+            </div>
+        )}
       </div>
     );
   }
