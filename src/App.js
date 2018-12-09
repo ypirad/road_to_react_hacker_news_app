253b295ff59c93c49d1f1a49e0a94dd8e624a84b
erikorojo <14225024+erikorojo@users.noreply.github.com>
2018-12-09 13:24:37 -0600

    Ch 2: Basics in React, Sec: Styling Components

--- src/App.js
+++ src/App.js
@@ -34,19 +34,26 @@ const Search = ({ value, onChange, children }) =>
       </form>;
 
 const Table = ({ list, pattern, onDismiss }) =>
-      <div>
+      <div className="table">
         {list.filter(isSearched(pattern)).map(
           item =>
-            <div key={item.objectID}>
-              <span>
+            <div key={item.objectID} className="table-row">
+              <span style={{ width: '40%' }}>
                 <a href={item.url}>{item.title}</a>
               </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
+              <span style={{ width : '30%' }}>
+                {item.author}
+              </span>
+              <span style={{ width : '10%' }}>
+                {item.num_comments}
+              </span>
+              <span style={{ width : '10%' }}>
+                {item.points}
+              </span>
+              <span style={{ width : '10%' }} >
                 <Button
                   onClick={() => onDismiss(item.objectID)}
+                  className="button-inline"
                   type="button"
                 >
                   Dismiss
@@ -100,13 +107,15 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
         <Table
           list={list}
           pattern={searchTerm}
