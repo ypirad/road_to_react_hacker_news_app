8b4bbb019b729f8c46a9a8c536a2794671282043
erikorojo <14225024+erikorojo@users.noreply.github.com>
2018-12-09 10:25:11 -0600

    Ch 2: Basics in React, Sec: Internal Component State

--- src/App.js
+++ src/App.js
@@ -21,19 +21,27 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item =>
-                  <div key={item.objectID}>
-                    <span>
-                      <a href={item.url}>{item.title}</a>
-                    </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                  </div>
-                 )}
+        {this.state.list.map(item =>
+                             <div key={item.objectID}>
+                               <span>
+                                 <a href={item.url}>{item.title}</a>
+                               </span>
+                               <span>{item.author}</span>
+                               <span>{item.num_comments}</span>
+                               <span>{item.points}</span>
+                             </div>
+                            )}
       </div>
     );
   }
