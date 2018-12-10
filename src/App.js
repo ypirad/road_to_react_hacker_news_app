f2667921ad5684290f9150ac37cc4169eb98fd60
erikorojo <14225024+erikorojo@users.noreply.github.com>
2018-12-10 08:34:42 -0600

    Ch 5: Advanced React Components, Sec: Ref a DOM Element

--- src/App.js
+++ src/App.js
@@ -12,20 +12,38 @@ const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
-const Search = ({ value, onChange, onSubmit, children }) =>
+class Search extends Component {
+  componentDidMount() {
+    if(this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const { value,
+            onChange,
+            onSubmit,
+            children
+          } = this.props;
+
+    return (
       <form onSubmit={onSubmit}>
         {children}
         <input
           type="text"
           value={value}
           onChange={onChange}
+          ref={(node) => { this.input = node; }}
         />
         <Button
           type="submit"
         >
           {children}
         </Button>
-      </form>;
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
